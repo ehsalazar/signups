82a231d4bfaea38daf926e9bcf5f39b522770dfd
Ernie Salazar <ehsalazar75@gmail.com>
2015-02-20 15:28:57 -0800

    Add user controller and view.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  include SessionsHelper
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      redirect_to :back
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :company_name, :phone_number, :email, :password)
+  end
+end
