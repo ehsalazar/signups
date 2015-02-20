12d173aed1415dec5ecbc62d1e68929e068bba87
Ernie Salazar <ehsalazar75@gmail.com>
2015-02-20 10:19:42 -0800

    Update Users migration with tbale information.

--- db/migrate/20150220181155_create_users.rb
+++ db/migrate/20150220181155_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :user_name
+      t.string :email
+      t.string :company_name
+      t.integer :phone_number
+      t.string :password_hash
+      t.timestamps
     end
   end
 end
