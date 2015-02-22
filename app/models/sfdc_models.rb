da609f473902eca9d96ecc33ed83a5d2191aa8f1
Ernie Salazar <ehsalazar75@gmail.com>
2015-02-22 13:59:34 -0800

    Update #populate_saleforce to use the #new constructor for needed attributes.

--- app/models/sfdc_models.rb
+++ app/models/sfdc_models.rb
@@ -2,17 +2,23 @@
 module SFDC_Models
   include Databasedotcom::Rails::Controller
 
-  def populate_salesforce(name)
+  def populate_salesforce(company)
     client = Databasedotcom::Client.new
     client.authenticate :username => ENV['SALESFORCE_USERNAME'], :password => ENV['SALESFORCE_PASSWORD']
     client.sobject_module = 'SFDC_Models'
     client.materialize('Account')
-    dummy = Account.new
-    dummy.Name = name
-    dummy.OwnerId = '005j000000BnwumAAB'
-    dummy.save
+    dummy = Account.new(Name: company.metadata['legal_name'], OwnerId: '005j000000BnwumAAB', Phone: company.users.first.phone_number, BillingCity: company.metadata['geo']['city'], BillingState: company.metadata['geo']['state'], BillingCountry: company.metadata['geo']['country'], NumberOfEmployees: company.metadata['employees'], Description: company.metadata['description'], Website: company.metadata['url'], Type: 'prospect')
+    account_id = dummy.save
   end
 end
 
-
-#Salesforce fields: account name, type: prospect, annual revenue, phone, fax, website, ticker symbol, employees, ownership, billing street, billing city, billing state, billing postal code, billing country, description
+# yahoo.Name = company.metadata['legal_name']
+# yahoo.OwnerId = '005j000000BnwumAAB'
+# yahoo.Phone = company.users.first.phone_number
+# yahoo.BillingCity = company.metadata['geo']['city']
+# yahoo.BillingState = company.metadata['geo']['state']
+# yahoo.BillingCountry = company.metadata['geo']['country']
+# yahoo.NumberOfEmployees = company.metadata['employees']
+# yahoo.Description = company.metadata['description']
+# yahoo.Website = company.metadata['url']
+# yahoo.Type = 'prospect'
