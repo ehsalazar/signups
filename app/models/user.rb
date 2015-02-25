b7843a3aa1da2c46c9c130b05464a2bd756e0a26
Ernie Salazar <ehsalazar75@gmail.com>
2015-02-24 17:25:18 -0800

    Update user model to validate presence of name and company name as well as presence and uniqueness of email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :user
   has_secure_password
+  validates :email, presence:true, uniqueness: true
+  validates :company_name, :name, presence: true
 end
