f5b4e5d4ec42335a199d3a9c62ae41b8d690bbb6
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-01 13:06:43 -0800

    Uncommented a needed line for after_fork.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end
 
-# after_fork do |server, worker|
+after_fork do |server, worker|
 #   Sidekiq.configure_client do |config|
 #     config.redis = { :size => 1 }
 #   end
